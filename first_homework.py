e249de5314a4a8fb2b84193b4f3f30bd19744316
ggjiuw <herezoom1231@gmail.com>
2023-08-01 16:28:43 +0300

    homework , and "#" removed from .gitignore

---
+++ first_homework.py
@@ -0,0 +1,10 @@
+# this is already the second homework, but the file is called that because I was asked
+
+country = 'Україна'
+
+association_country = '\U0001F60E' #😎
+
+result = country + ' - ' + association_country
+
+print(result)
+da
